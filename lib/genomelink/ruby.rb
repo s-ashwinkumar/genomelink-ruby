c3b26413d3e3536ff05c46a48f54a7cd2872c71a
Ashwin Subramanian <Ashwin.Subramanian@ooma.com>
2018-03-10 12:43:29 -0800

    First commit

---
+++ lib/genomelink/ruby.rb
@@ -0,0 +1,7 @@
+require "genomelink/ruby/version"
+
+module Genomelink
+  module Ruby
+    # Your code goes here...
+  end
+end
---
+++ spec/genomelink/ruby_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Genomelink::Ruby do
+  it "has a version number" do
+    expect(Genomelink::Ruby::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
